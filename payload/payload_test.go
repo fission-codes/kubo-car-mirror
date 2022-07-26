c0a724150642d1f7d035e58bddcf6f5bc8780a9e
Justin Johnson <justin@justinjohnson.io>
2022-07-26 12:43:04 -0500

    Payload tests

--- payload/payload_test.go
+++ payload/payload_test.go
@@ -10,36 +10,54 @@ import (
 
 func TestEncodeDecode(t *testing.T) {
 
-	f := bloom.New(100, 6)
+	f := bloom.New(128, 6)
 	f.Add([]byte("one"))
 	f.Add([]byte("two"))
 	f.Add([]byte("three"))
 	f.Add([]byte("four"))
 	bb := f.Bytes()
-	// assert.NilError(t, err, "Failed to marshal binary from bitset")
 
-	d := PullRequestor{RS: []string{"a", "b", "c"}, BK: 8, BB: bb}
-	de, err := Encode(d)
+	// Pull requestor
+	pullRequest := PullRequestor{RS: []string{"a", "b", "c"}, BK: 6, BB: bb}
+	pullRequestEncoded, err := Encode(pullRequest)
 	assert.NilError(t, err)
 
-	var dd PullRequestor
-	if err := Decode(de, &dd); err != nil {
+	var pullRequestDecoded PullRequestor
+	if err := Decode(pullRequestEncoded, &pullRequestDecoded); err != nil {
 		t.Error(err)
 	} else {
-		assert.Assert(t, reflect.DeepEqual(d.RS, dd.RS), "must be equal")
-		assert.Assert(t, reflect.DeepEqual(d.BK, dd.BK), "must be equal")
-		assert.Assert(t, reflect.DeepEqual(d.BB, dd.BB), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pullRequest.RS, pullRequestDecoded.RS), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pullRequest.BK, pullRequestDecoded.BK), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pullRequest.BB, pullRequestDecoded.BB), "must be equal")
 	}
 
-	// bbEncoded, _ := Encode(bb)
-	// err = os.WriteFile("/tmp/bloom1.cbor", bbEncoded, 0644)
-	// if err != nil {
-	// 	fmt.Printf("Failed to write file.  err: %v\n", err)
-	// }
+	// Pull provider is just CARv1
 
-	// encodedStr := hex.EncodeToString(bb)
+	// Push Requestor
+	pushRequest := PushRequestor{BK: 6, BB: bb, PL: nil}
+	pushRequestEncoded, err := Encode(pushRequest)
+	assert.NilError(t, err)
 
-	// fmt.Printf("Binary BB = %b\n", bb)
-	// fmt.Printf("Hex BB    = %s\n", encodedStr)
-	// fmt.Printf("CBOR BB   = %b\n", bbEncoded)
+	var pushRequestDecoded PushRequestor
+	if err := Decode(pushRequestEncoded, &pushRequestDecoded); err != nil {
+		t.Error(err)
+	} else {
+		assert.Assert(t, reflect.DeepEqual(pushRequest.BK, pushRequestDecoded.BK), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pushRequest.BB, pushRequestDecoded.BB), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pushRequest.PL, pushRequestDecoded.PL), "must be equal")
+	}
+
+	// Push Provider
+	pushProvide := PushProvider{SR: []string{"a", "b", "c"}, BK: 6, BB: bb}
+	pushProvideEncoded, err := Encode(pushProvide)
+	assert.NilError(t, err)
+
+	var pushProvideDecoded PushProvider
+	if err := Decode(pushProvideEncoded, &pushProvideDecoded); err != nil {
+		t.Error(err)
+	} else {
+		assert.Assert(t, reflect.DeepEqual(pushProvide.SR, pushProvideDecoded.SR), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pushProvide.BK, pushProvideDecoded.BK), "must be equal")
+		assert.Assert(t, reflect.DeepEqual(pushProvide.BB, pushProvideDecoded.BB), "must be equal")
+	}
 }
