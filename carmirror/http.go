6f87eeabfe9636adab8a82087c415b84fe1364c6
Justin Johnson <justin@justinjohnson.io>
2022-11-09 10:19:55 -0600

    force build

--- carmirror/http.go
+++ carmirror/http.go
@@ -213,6 +213,7 @@ func (cm *CarMirror) HTTPRemotePushHandler() http.HandlerFunc {
 		}
 		n := uint64(len(bloomCids) * 8)
 		// providerGraphConfirmation = bloom.NewFilterWithEstimates(n, bloom.EstimateFPP(n))
+		// TODO: Fix
 		providerGraphConfirmation = bloom.NewFilterWithEstimates(n, 0.001)
 		for _, cid := range bloomCids {
 			providerGraphConfirmation.Add(cid.Bytes())
